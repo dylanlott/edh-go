8b90cfbb530bb01130536fb91a192495e3d8ead3
Dylan Lott <lott.dylan@gmail.com>
2020-04-14 15:44:07 -0600

    removes main.go

--- main.go
+++ main.go
@@ -2,33 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-
-	socketio "github.com/googollee/go-socket.io"
 )
 
 func main() {
-	server, err := socketio.NewServer(nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	server.OnConnect("/", func(s socketio.Conn) error {
-		s.SetContext("")
-		fmt.Println("connected:", s.ID())
-		return nil
-	})
-	server.OnError("/", func(s socketio.Conn, err error) {
-		fmt.Println("ERRSOCKET:", err)
-	})
-	server.OnDisconnect("/", func(s socketio.Conn, reason string) {
-		fmt.Println("DISCONNECT:", reason)
-	})
-	go server.Serve()
-	defer server.Close()
-
-	http.Handle("/socket.io/", server)
-	http.Handle("/", http.FileServer(http.Dir("./web/dist/")))
-	log.Println("Serving at localhost:9090...")
-	log.Fatal(http.ListenAndServe(":9090", nil))
+	fmt.Printf("edh-go")
 }
